1ada5eb849f1a747ab7da0624d3b7668ef61385b
akande56 <abdulsalamabubakar52@gmail.com>
2025-01-06 15:49:20 +0100

    update..

--- config/settings/base.py
+++ config/settings/base.py
@@ -46,7 +46,12 @@ LOCALE_PATHS = [str(BASE_DIR / "locale")]
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES = {"postgresql://postgres:krakof1998..,,@db.mgqhgjuqtddlvqrmfdvp.supabase.co:5432/postgres": env.db("DATABASE_URL")}
+DATABASES = {
+    "default": env.db(
+        "DATABASE_URL",
+        default="postgres:///my_awesome_project",
+    ),
+}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
